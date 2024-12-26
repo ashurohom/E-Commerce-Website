ee0d4e07f219ef2794235dcb3c3f555044079042
ashurohom <ashitosh.rohom@gmail.com>
2024-12-26 22:13:15 +0530

    comment

--- ecom/myapp/admin.py
+++ ecom/myapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Product
 # Register your models here.
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):   # show the product table to admin
     list_display=['id','pname','price']
 admin.site.register(Product,ProductAdmin)  
 
